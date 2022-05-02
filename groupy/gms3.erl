723cece153f7d7ccafe60a69829494bd15fef9ed
Albert Capdevila <albert.capde00@gmail.com>
2022-05-02 19:42:03 +0200

    groupy done

--- groupy/gms3.erl
+++ groupy/gms3.erl
@@ -1,4 +1,4 @@
--module(gms2).
+-module(gms3).
 -export([start/1, start/2]).
 -define(timeout, 1000).
 -define(arghh, 100).
@@ -9,7 +9,7 @@ start(Name) ->
     spawn_link(fun()-> init(Name, Self) end).
 
 init(Name, Master) ->
-    leader(Name, Master, []).
+    leader(Name, Master, [], 0).
 
 start(Name, Grp) ->
     Self = self(),
@@ -19,24 +19,24 @@ init(Name, Grp, Master) ->
     Self = self(), 
     Grp ! {join, Self},
     receive
-        {view, Leader, Slaves} ->
+        {view, N, Leader, Slaves} ->
             Ref = erlang:monitor(process, Leader),
             Master ! joined,
-            slave(Name, Master, Leader, Slaves, Ref)
+            slave(Name, Master, Leader, Slaves, Ref, N + 1, {view, N, Leader, Slaves})
         after ?timeout ->
             Master ! {error, "no reply from leader"}
     end.
 
-leader(Name, Master, Slaves) ->    
+leader(Name, Master, Slaves, N) ->    
     receive
         {mcast, Msg} ->
-            bcast(Name, {msg, Msg}, Slaves),
+            bcast(Name, {msg, N, Msg}, Slaves),
             Master ! {deliver, Msg}, 
-            leader(Name, Master, Slaves);
+            leader(Name, Master, Slaves, N + 1);
         {join, Peer} ->
             NewSlaves = lists:append(Slaves, [Peer]),           
-            bcast(Name, {view, self(), NewSlaves}, NewSlaves),
-            leader(Name, Master, NewSlaves);
+            bcast(Name, {view, N, self(), NewSlaves}, NewSlaves),
+            leader(Name, Master, NewSlaves, N + 1);
         stop ->
             ok;
         Error ->
@@ -62,38 +62,43 @@ crash(Name, Msg) ->
             ok
 end.
 
-slave(Name, Master, Leader, Slaves, Ref) ->    
+slave(Name, Master, Leader, Slaves, Ref, N, Last) ->    
     receive
         {mcast, Msg} ->
             Leader ! {mcast, Msg},
-            slave(Name, Master, Leader, Slaves, Ref);
+            slave(Name, Master, Leader, Slaves, Ref, N, Last);
         {join, Peer} ->
             Leader ! {join, Peer}, 
-            slave(Name, Master, Leader, Slaves, Ref);
-        {msg, Msg} ->
+            slave(Name, Master, Leader, Slaves, Ref, N, Last);
+        {msg, I, _} when I < N ->
+            slave(Name, Master, Leader, Slaves, Ref, N, Last);
+        {msg, N, Msg} ->
             Master ! {deliver, Msg},
-            slave(Name, Master, Leader, Slaves, Ref);
-        {view, Leader, NewSlaves} ->
-            slave(Name, Master, Leader, NewSlaves, Ref);
-        {view, NewLeader, NewSlaves} ->
+            slave(Name, Master, Leader, Slaves, Ref, N + 1, {msg, N, Msg});
+        {view, I, _, _} when I < N ->
+            slave(Name, Master, Leader, Slaves, Ref, N, Last);    
+        {view, N, Leader, NewSlaves} ->
+            slave(Name, Master, Leader, NewSlaves, Ref, N + 1, {view, N, Leader, NewSlaves});
+        {view, N, NewLeader, NewSlaves} ->
             erlang:demonitor(Ref, [flush]),
             NewRef = erlang:monitor(process, NewLeader),
-            slave(Name, Master, NewLeader, NewSlaves, NewRef);
+            slave(Name, Master, NewLeader, NewSlaves, NewRef, N + 1, {view, N, NewLeader, NewSlaves});
         {'DOWN', _Ref, process, Leader, _Reason} ->
-            election(Name, Master, Slaves);
+            election(Name, Master, Slaves, N, Last);
         stop ->
             ok;
         Error ->
             io:format("slave ~s: strange message ~w~n", [Name, Error])
     end.
 
-election(Name, Master, Slaves) ->
+election(Name, Master, Slaves, N, Last) ->
     Self = self(),
     case Slaves of
         [Self|Rest] ->
-            bcast(Name, {view, Self, Rest}, Rest),
-            leader(Name, Master, Rest);
+            bcast(Name, Last, Rest),
+            bcast(Name, {view, N, Self, Rest}, Rest),
+            leader(Name, Master, Rest, N + 1);
         [NewLeader|Rest] ->
             MonitorRef = erlang:monitor(process, NewLeader),
-            slave(Name, Master, NewLeader, Rest, MonitorRef)
+            slave(Name, Master, NewLeader, Rest, MonitorRef, N, Last)
     end.
