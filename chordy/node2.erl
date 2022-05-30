a1e1438f8445cf42fa36281a918ea5987220ff9a
Albert Capdevila <albert.capde00@gmail.com>
2022-05-30 19:38:43 +0200

    chordy done

--- chordy/node2.erl
+++ chordy/node2.erl
@@ -52,13 +52,13 @@ node(MyKey, Predecessor, Successor, Store) ->
         stop ->
             ok;
         probe ->
-            create_probe(MyKey, Successor),
+            create_probe(MyKey, Successor, Store),
             node(MyKey, Predecessor, Successor, Store);
         {probe, MyKey, Nodes, T} ->
             remove_probe(MyKey, Nodes, T),
             node(MyKey, Predecessor, Successor, Store);
         {probe, RefKey, Nodes, T} ->
-            forward_probe(MyKey, RefKey, [MyKey|Nodes], T, Successor),
+            forward_probe(MyKey, RefKey, [MyKey|Nodes], T, Successor, Store),
             node(MyKey, Predecessor, Successor, Store);
         {add, Key, Value, Qref, Client} ->
             Added = add(Key, Value, Qref, Client,
@@ -120,18 +120,18 @@ notify({Nkey, Npid}, MyKey, Predecessor, Store) ->
             end
     end.
 
-create_probe(MyKey, {_, Spid}) ->
+create_probe(MyKey, {_, Spid}, Store) ->
     Spid ! {probe, MyKey, [MyKey], erlang:monotonic_time()},
-    io:format("Node ~w created probe~n", [MyKey]).
+    io:format("Node ~w created probe, Store: ~w~n", [MyKey, Store]).
     
 remove_probe(MyKey, Nodes, T) ->
     T2 = erlang:monotonic_time(),
     Time = erlang:convert_time_unit(T2-T, native, microsecond),
     io:format("Node ~w received probe after ~w us -> Ring: ~w~n", [MyKey, Time, Nodes]).
 
-forward_probe(MyKey, RefKey, Nodes, T, {_, Spid}) ->
+forward_probe(MyKey, RefKey, Nodes, T, {_, Spid}, Store) ->
     Spid ! {probe, RefKey, Nodes, T},
-    io:format("Node ~w forwarded probe started by node ~w~n", [MyKey, RefKey]).
+    io:format("Node ~w forwarded probe started by node ~w, Store: ~w~n", [MyKey, RefKey, Store]).
 
 add(Key, Value, Qref, Client, _, nil, {_, Spid}, Store) ->
     Spid ! {add, Key, Value, Qref, Client},
--- chordy/node4.erl
+++ chordy/node4.erl
@@ -1,4 +1,4 @@
--module(node3).
+-module(node4).
 -export([start/1, start/2]).
 
 -define(Stabilize, 1000).
@@ -15,7 +15,7 @@ init(MyKey, PeerPid) ->
     Predecessor = nil,
     {ok, Successor} = connect(MyKey, PeerPid),
     schedule_stabilize(),    
-    node(MyKey, Predecessor, Successor, nil, storage:create()).
+    node(MyKey, Predecessor, Successor, nil, storage:create(), storage:create()).
 
 connect(MyKey, nil) ->
     {ok, {MyKey, nil, self()}};
@@ -33,47 +33,57 @@ connect(_, PeerPid) ->
 schedule_stabilize() ->
     timer:send_interval(?Stabilize, self(), stabilize).
 
-node(MyKey, Predecessor, Successor, Next, Store) ->
+node(MyKey, Predecessor, Successor, Next, Store, Replica) ->
     receive 
         {key, Qref, Peer} ->
             Peer ! {Qref, MyKey},
-            node(MyKey, Predecessor, Successor, Next, Store);
+            node(MyKey, Predecessor, Successor, Next, Store, Replica);
         {notify, NewPeer} ->
             {NewPredecessor, NewStore} = notify(NewPeer, MyKey, Predecessor, Store),
-            node(MyKey, NewPredecessor, Successor, Next, NewStore);
+             {_, _, Spid} = Successor,
+            Spid ! {pushreplica, NewStore},
+            node(MyKey, NewPredecessor, Successor, Next, NewStore, Replica);
         {request, Peer} ->
             request(Peer, Predecessor, Successor),
-            node(MyKey, Predecessor, Successor, Next, Store);
+            node(MyKey, Predecessor, Successor, Next, Store, Replica);
         {status, Pred, Nx} ->
             {NewSuccessor, NewNext} = stabilize(Pred, Nx, MyKey, Successor),
-            node(MyKey, Predecessor, NewSuccessor, NewNext, Store);
+            node(MyKey, Predecessor, NewSuccessor, NewNext, Store, Replica);
         stabilize ->
             stabilize(Successor),
-            node(MyKey, Predecessor, Successor, Next, Store);
+            node(MyKey, Predecessor, Successor, Next, Store, Replica);
         stop ->
             ok;
         probe ->
-            create_probe(MyKey, Successor, Store),
-            node(MyKey, Predecessor, Successor, Next, Store);
+            create_probe(MyKey, Successor, Store, Replica),
+            node(MyKey, Predecessor, Successor, Next, Store, Replica);
         {probe, MyKey, Nodes, T} ->
             remove_probe(MyKey, Nodes, T),
-            node(MyKey, Predecessor, Successor, Next, Store);
+            node(MyKey, Predecessor, Successor, Next, Store, Replica);
         {probe, RefKey, Nodes, T} ->
-            forward_probe(MyKey, RefKey, [MyKey|Nodes], T, Successor, Store),
-            node(MyKey, Predecessor, Successor, Next, Store);
+            forward_probe(MyKey, RefKey, [MyKey|Nodes], T, Successor, Store, Replica),
+            node(MyKey, Predecessor, Successor, Next, Store, Replica);
         {add, Key, Value, Qref, Client} ->
             Added = add(Key, Value, Qref, Client,
                         MyKey, Predecessor, Successor, Store),
-            node(MyKey, Predecessor, Successor, Next, Added);
+            node(MyKey, Predecessor, Successor, Next, Added, Replica);
         {lookup, Key, Qref, Client} ->
             lookup(Key, Qref, Client, MyKey, Predecessor, Successor, Store),
-            node(MyKey, Predecessor, Successor, Next, Store);
+            node(MyKey, Predecessor, Successor, Next, Store, Replica);
         {handover, Elements} ->
             Merged = storage:merge(Store, Elements),
-            node(MyKey, Predecessor, Successor, Next, Merged);
+            {_, _, Spid} = Successor,
+            Spid ! {pushreplica, Merged},
+            node(MyKey, Predecessor, Successor, Next, Merged, Replica);
         {'DOWN', Ref, process, _, _} ->
-            {NewPred, NewSucc, NewNext} = down(Ref, Predecessor, Successor, Next),
-            node(MyKey, NewPred, NewSucc, NewNext, Store)
+            {NewPred, NewSucc, NewNext, NewStore, NewReplica} = down(Ref, Predecessor, Successor, Next, Store, Replica),
+            node(MyKey, NewPred, NewSucc, NewNext, NewStore, NewReplica);
+        {replicate, Key, Value, Client, Qref} ->
+            Added = storage:add(Key, Value, Replica),
+            Client ! {Qref, ok},
+            node(MyKey, Predecessor, Successor, Next, Store, Added);
+        {pushreplica, NewReplica} ->
+            node(MyKey, Predecessor, Successor, Next, Store, NewReplica)
    end.
 
 stabilize(Pred, Nx, MyKey, Successor) ->
@@ -129,18 +139,18 @@ notify({Nkey, Npid}, MyKey, Predecessor, Store) ->
             end
     end.
 
-create_probe(MyKey, {_, _, Spid}, Store) ->
+create_probe(MyKey, {_, _, Spid}, Store, Replica) ->
     Spid ! {probe, MyKey, [MyKey], erlang:monotonic_time()},
-    io:format("Node ~w created probe, Store: ~w~n", [MyKey, Store]).
+    io:format("Node ~w created probe, Store: ~w, Replica ~w~n", [MyKey, Store, Replica]).
     
 remove_probe(MyKey, Nodes, T) ->
     T2 = erlang:monotonic_time(),
     Time = erlang:convert_time_unit(T2-T, native, microsecond),
     io:format("Node ~w received probe after ~w us -> Ring: ~w~n", [MyKey, Time, Nodes]).
 
-forward_probe(MyKey, RefKey, Nodes, T, {_, _, Spid}, Store) ->
+forward_probe(MyKey, RefKey, Nodes, T, {_, _, Spid}, Store, Replica) ->
     Spid ! {probe, RefKey, Nodes, T},
-    io:format("Node ~w forwarded probe started by node ~w, Store: ~w~n", [MyKey, RefKey, Store]).
+    io:format("Node ~w forwarded probe started by node ~w, Store: ~w, Replica: ~w~n", [MyKey, RefKey, Store, Replica]).
 
 add(Key, Value, Qref, Client, _, nil, {_, _,Spid}, Store) ->
     Spid ! {add, Key, Value, Qref, Client},
@@ -149,7 +159,7 @@ add(Key, Value, Qref, Client, MyKey, {Pkey,_, _}, {_, _,Spid}, Store) ->
     case key:between(Key , Pkey , MyKey) of 
         true ->
             Added = storage:add(Key, Value, Store), 
-            Client ! {Qref, ok},
+            Spid ! {replicate, Key, Value,  Client, Qref},
             Added;
         false ->
             Spid ! {add, Key, Value, Qref, Client},
@@ -180,10 +190,13 @@ demonit(nil) ->
 demonit(MonitorRef) ->
     erlang:demonitor(MonitorRef, [flush]).
 
-down(Ref, {_, Ref, _}, Successor, Next) ->
-    {nil, Successor, Next};
-down(Ref, Predecessor, {_, Ref, _}, {Nkey, Npid}) ->
+down(Ref, {_, Ref, _}, Successor, Next, Store, Replica) ->
+    NewStore = storage:merge(Store, Replica),
+    {_, _, Spid} = Successor,
+    Spid ! {pushreplica, NewStore},
+    {nil, Successor, Next, NewStore, storage:create()};
+down(Ref, Predecessor, {_, Ref, _}, {Nkey, Npid}, Store, Replica) ->
     Nref = monit(Npid),
     self() ! stabilize,
-    {Predecessor, {Nkey, Nref, Npid}, nil}.
+    {Predecessor, {Nkey, Nref, Npid}, nil, Store, Replica}.
     
